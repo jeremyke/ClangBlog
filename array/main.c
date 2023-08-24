6dbfc8af3eca22f60110fcfce81e7fe87893a5eb
jeremyke <2287111230@qq.com>
2023-08-24 16:46:20 +0800

    feat:数组和函数

--- array/main.c
+++ array/main.c
@@ -22,10 +22,41 @@ void btc(){
     printf("平均数为：%.2f",res);
 }
 
+void arrayV2(){
+    int arr1[10] = {};
+    printf("arr[0]=%d;arr[1]=%d",arr1[0],arr1[2]);
+}
+
+
+void arrayV3(int arr[],int count){
+    printf("arr[3]=%d\n",arr[2]);
+    printf("arr的长度为=%d\n",count);
+    arr[2] = 10;
+    printf("arr[2]=%d\n",arr[2]);
+}
+
+void printArray(int array[]){
+    printf("printArray size = %lu\n", sizeof(array)); // 8
+    int length = sizeof(array)/ sizeof(int); // 2
+    printf("length = %d", length);
+}
+
+void callArrFun(){
+    int arr[5] = {1,4,3,7,13};
+    printf("arr[2]=%d\n",arr[2]);
+    arrayV3(arr,5);
+    printf("arr[2]=%d\n",arr[2]);
+    printArray(arr);
+}
+
+
 
 
 int main(){
     //array();
-    btc();
+    //btc();
+    //arrayV2();
+    callArrFun();
+
     return 0;
 }
