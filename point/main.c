62a58ef07364b6f1c996a964cd7d2e0c021ed207
jeremyke <2287111230@qq.com>
2023-08-25 17:46:24 +0800

    feat:指针

--- point/main.c
+++ point/main.c
@@ -2,6 +2,7 @@
 // Created by 松夏 on 2023/8/24.
 //
 #include <stdio.h>
+#include <string.h>
 
 void printAddress(){
     int num = 6;//占用4个字节，假设地址为：0x7ff7ba4f63ec
@@ -29,9 +30,93 @@ void point2(){
 
 }
 
+void point3(){
+    char c='a';
+    char *cp;
+    cp = &c;
+    char **cp2;
+    cp2 = &cp;
+    printf("c=%c",**cp2);//a
+}
+
+void change(){
+    int a = 1;
+    int b = 2;
+    int *ap = &a;
+    int *bp = &b;
+
+    int tmp;
+    tmp = *ap;
+    *ap = *bp;
+    *bp = tmp;
+    printf("a=%zu,b=%zu",a,b);
+}
+
+void jishuan(int a, int b,int *sub,int *add){
+    *add = a + b;
+    *sub = a-b;
+}
+
+void callFun(){
+    int a = 2;
+    int b = 5;
+    int sub = 0;
+    int add = 0;
+    jishuan(a,b,&sub,&add);
+    printf("sub=%d;sum=%d\n",sub,add);
+}
+
+void pointArr(){
+    int a[3] = {3,7,9};
+    printf("%p\n%p",&(a[0]),a);//输出结果相同
+}
+
+void pintArr2(){
+    int a[3] = {2,6,4};
+    printf("a[2]=%d\n",a[2]);//下标法
+    int *p = a;
+    printf("a[2]=%d\n",*(p+2));//指针法
+}
+
+void printArr3(){
+    char ch[] = "hello";
+    printf("%s\n",ch);
+}
+
+
+void printArr4(){
+    char *str="abc";
+    printf("%s\n",str);
+}
+
+void printArr5(){
+    char *myStr = "i love you!";
+    for(int i=0;i< strlen(myStr);i++){
+        printf("myStr[i]=%c\n",myStr[i]);
+    }
+}
+
+int printArr6(int a,int b){
+    return a + b;
+}
+
+void callPrintArr6(){
+    int(*p)(int,int);
+    p = printArr6;
+}
+
 
 void main(){
     //printAddress();
     //point();
-    point2();
+    //point2();
+    //point3();
+    //change();
+    //callFun();
+    //pointArr();
+    //pintArr2();
+    //printArr3();
+    //printArr4();
+    printArr5();
+
 }
