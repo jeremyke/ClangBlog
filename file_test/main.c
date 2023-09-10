91a65058ca21d214b44686f503ad087ca7d7f7d9
jeremyke <2287111230@qq.com>
2023-09-10 22:37:43 +0800

    feat:文件读取

--- file_test/main.c
+++ file_test/main.c
@@ -19,11 +19,51 @@ void fileTest2(){
     fclose(fb);
 }
 
+void fileTest3(){
+    FILE *fb = fopen("./file_test/bin.txt","w");
+    fclose(fb);
+}
 
+
+void fileTest4(){
+    FILE *fb = fopen("./file_test/bin.txt","w");
+    //2.往文件中写入内容
+    for(char ch='a'; ch<='z';ch++){
+        //一次写入一个字符
+        char res=fputc(ch,fb);
+        printf("res=%c\n",res);
+    }
+    fclose(fb);
+}
+
+void fileTest5(){
+    FILE *fb = fopen("./file_test/bin.txt","r+");
+    //2.从文件中读取内容
+    char res=EOF;
+    while((res=fgetc(fb))!=EOF){
+        printf("res=%c\n",res);
+    }
+    fclose(fb);
+}
+
+void fileTest6(){
+    FILE *fb = fopen("./file_test/bin.txt","r+");
+    //2.从文件中读取内容
+    char res=EOF;
+    //注意:由于只有先读了才会修改标志位,
+    // 所以通过feof判断是否到达文件末尾,一定要先读再判断,不能先判断再读
+    while((res=fgetc(fb))&&(!feof(fb))){
+        printf("res=%c\n",res);
+    }
+    fclose(fb);
+}
 
 
 int main(){
-    fileTest1();
-    fileTest2();
+    //fileTest1();
+    //fileTest2();
+    //fileTest4();
+    //fileTest5();
+    fileTest6();
     return 0;
 }
