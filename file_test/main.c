f35287d9925a5852e79cedef9f29f0d8448dc47b
jeremyke <2287111230@qq.com>
2023-09-11 21:29:15 +0800

    feat:文件读取

--- file_test/main.c
+++ file_test/main.c
@@ -58,12 +58,71 @@ void fileTest6(){
     fclose(fb);
 }
 
+void fileTest7(){
+    FILE *fb = fopen("./file_test/ccc.txt","w+");
+    fputs("asasds\n",fb);
+    fputs("ssssddds\0自动终止写入",fb);//遇到\0自动终止写入
+    fclose(fb);
+}
+
+void fileTest8(){
+    FILE *fb = fopen("./file_test/08.txt","w+");
+    fputs("qwerty\nuiop\n",fb);
+    fputs("asdfghjkl\0",fb);//遇到\0自动终止写入
+
+    //将FILE结构体中的读写指针重新移动到最前面
+    // 注意:FILE结构体中读写指针每读或写一个字符后都会往后移动
+    rewind(fb);
+    char str[1024];
+    //从fp中读取4个字符,存入到str中
+    //最多只能读取N-1个字符,会在最后自动添加\0
+    fgets(str,4,fb);
+    printf("str=%s\n",str);//qwe
+
+    //遇到\n自动结束
+    rewind(fb);
+    char str1[1024];
+    fgets(str1,10,fb);
+    printf("str1=%s\n",str1);//qwerty
+
+    //读取到EOF自动结束
+    rewind(fb);
+    char str2[1024];
+    while(fgets(str2,1024,fb)){
+        printf("str2=%s",str2);
+    }
+    fclose(fb);
+}
+
+
+void fileTest9(){
+    FILE *fp = fopen("09.txt","w+");
+    //注意:fputs不会自动添加\n
+    fputs("12345678910\n",fp);
+    fputs("12345678910\n",fp);
+    fputs("12345678910",fp);
+    //将FILE结构体中的读写指针重新移动到最前面
+    // 注意:FILE结构体中读写指针每读或写一个字符后都会往后移动
+    rewind(fp);
+    char str[1024];
+    //每次从fp中读取1024个字符,存入到str中
+    // 读取到文件末尾自动结束
+    while(fgets(str,1024,fp)&&!feof(fp)){
+        printf("str=%s",str);
+    }
+    fclose(fp);
+}
+
+
 
 int main(){
     //fileTest1();
     //fileTest2();
     //fileTest4();
     //fileTest5();
-    fileTest6();
+    //fileTest6();
+    //fileTest7();
+    fileTest8();
+    //fileTest9();
     return 0;
 }
